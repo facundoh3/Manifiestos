822234319b1cf17e90f331435119a6357ba72666
facundoh3 <facundoh69@gmail.com>
2025-05-02 18:50:44 -0300

    refactor: cambios en el deployment, buenas practicas recomendadas por el profe

--- deployment.sh
+++ deployment.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 [ -f config.env ] && source config.env
-: "${MANIFESTS_DIR:="."}"
-: "${PROFILE:="0311at"}"
+: "${MANIFESTS_DIR:=.}"
+: "${PROFILE:=0311at}"
 
 cd "$MANIFESTS_DIR"
 
@@ -14,8 +14,6 @@ else
   minikube start -p "$PROFILE"
 fi
 
-
-#Esto no lo sabia como hacer asique le pregunte al chat
 if ! kubectl get pv web-static-pv >/dev/null 2>&1; then
   echo "Aplicando PV y PVC…"
   kubectl apply -f volumes/pv.yml
@@ -23,7 +21,6 @@ if ! kubectl get pv web-static-pv >/dev/null 2>&1; then
 else
   echo "PV/PVC ya existen, no lo voy a hacer de nuevo"
 fi
-
 
 if kubectl get deployment web-static-deployment >/dev/null 2>&1; then
   echo "Actualizando el deployment con rollout-restart"
@@ -34,12 +31,32 @@ else
   kubectl apply -f services/web-service.yml
 fi
 
-kubectl rollout status deployment/web-static-deployment --timeout=60s
+kubectl rollout status deployment/web-static-deployment --timeout=120s
 
-echo "URL del servicio:"
-minikube service web-service -p "$PROFILE" --url
+echo
+POD_NAME=$(kubectl get pods -l app=webapp -o jsonpath='{.items[0].metadata.name}')
+echo "> Comprobando el volumen dentro del pod"
+if kubectl exec "$POD_NAME" -- sh -c 'mount | grep -q /usr/share/nginx/html'; then
+  echo "Volumen montado"
+else
+  echo "El volumen no está montado en /usr/share/nginx/html" >&2
+  exit 1
+fi
+
+echo
+HTTP_CODE=$(kubectl exec "$POD_NAME" -- sh -c 'curl -s -o /dev/null -w "%{http_code}" http://localhost')
+if [ "$HTTP_CODE" -eq 200 ]; then
+  echo "La página está siendo servida correctamente en el Pod"
+else
+  echo "$HTTP_CODE - fallo al servir la página dentro del Pod" >&2
+  exit 1
+fi
+
+pkill -f "kubectl port-forward svc/web-service 8080:80" >/dev/null 2>&1 || true
+kubectl port-forward svc/web-service 8080:80 >/dev/null 2>&1 &
+echo "Ingresa a tu sitio en: http://localhost:8080"
 
 echo "Si cambias el contenido volve a ejecutar ./deployment.sh"
-echo '  (\\ /)'
+echo '  (\ /)'
 echo '  ( . .)'
 echo '  (" )( ")'
